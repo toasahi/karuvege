52ec97da2112b53ec8d593e25cde14d5b6275ae3
toasahi <asa1313to@gmail.com>
2022-11-01 14:46:52 +0900

    update:LCD点灯

--- Karuvege.ino
+++ Karuvege.ino
@@ -1,9 +1,24 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+ 
+const unsigned int ADDRESS = 0x27;
+const int CHARS_NUM = 16;
+const int LINES_NUM = 2;
+ 
+LiquidCrystal_I2C lcd(ADDRESS, CHARS_NUM, LINES_NUM);
+ 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  lcd.init();
+  lcd.backlight();
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  lcd.setCursor(0, 0);
+  lcd.print("Hello World!");
+ 
+  lcd.setCursor(0, 1);
+  lcd.print("DISP TEST");
+ 
+  delay(100);
 }
