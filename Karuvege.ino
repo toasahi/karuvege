c8578dbed424db48502c46b720cb56eb0c2720f0
toasahi <asa1313to@gmail.com>
2022-11-08 14:42:01 +0900

    commit:LCDに日付、曜日を表示

--- Karuvege.ino
+++ Karuvege.ino
@@ -1,23 +1,69 @@
 #include <Wire.h>
 #include <ST7032.h>
+#include <HTTPClient.h>
+#define JST 3600*9 //日本標準時子午線
+#define uS_TO_S_FACTOR 1000000ULL //マイクロ->秒に変換係数
+
+//Wifi
+HTTPClient http;
+WiFiClientSecure client;
+
+//WiFi設定
+const char ssid[] = "C0p2Ec2-WLAN";
+const char password[] = "4Emah5LdS";
 
 ST7032 lcd;
 
 void setup() {
+  Serial.begin(115200);
+  //WiFi接続
+  Serial.print("Connecting to");
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  int wifi_count = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    //通信状況が悪い時リスタート
+    if (++wifi_count > 30) {
+      Serial.print("リスタート");
+    }
+  }
+  Serial.println("Connected");
+
+  //日本時間の設定
+  configTime( JST, 0, "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");
+  configTzTime("JST-9", "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");
+
   // LCD表示領域設定(8桁, 2行)
   lcd.begin(8, 2);
 
   // コントラスト設定(0〜63)
   lcd.setContrast(30);
-
-  // (1行目)
-  lcd.setCursor(0, 0);
-  lcd.print("I Love");
-
-  // (2行目)
-  lcd.setCursor(0, 1);
-  lcd.print("Arduino!");
+  
 }
 
 void loop() {
+  time_t t;
+  struct tm *tm;
+  static const char *wd[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+  t = time(NULL);
+  tm = localtime(&t);
+  Serial.printf(" %04d/%02d/%02d(%s) %02d:%02d:%02d\n",
+                tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
+                wd[tm->tm_wday],
+                tm->tm_hour, tm->tm_min, tm->tm_sec);
+
+  unsigned long currentHour = tm->tm_hour;
+  unsigned long currentMin = tm->tm_min;
+  
+  lcd.setCursor(0, 0);
+  lcd.print(currentHour);
+  lcd.print(":");
+  lcd.print(currentMin);
+
+  
+  // (2行目)
+  lcd.setCursor(0, 1);
+  lcd.print(wd[tm->tm_wday]);
 }
