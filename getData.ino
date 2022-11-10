33efdfaa02785978b542b3e3e812a578adf62898
toasahi <asa1313to@gmail.com>
2022-11-10 10:07:53 +0900

    commit:指摘内容を訂正しました

--- getData.ino
+++ getData.ino
@@ -1,11 +1,14 @@
-#include "getData.h"
+#include "Getdata.h"
 
 unsigned long getTime(String label) {
   time_t t;
   struct tm *tm;
   t = time(NULL);
   tm = localtime(&t);
-  if (label == "hour") return tm->tm_hour;
-  if (label == "min") return tm->tm_min;
+  if (label == "hour") {
+    return tm->tm_hour;
+  } else if (label == "min") {
+    return tm->tm_min;
+  };
   return tm->tm_wday;
 }
