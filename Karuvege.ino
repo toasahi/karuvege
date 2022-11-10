c884a7749ee3972fbc63d03b30c484f009ba18a3
toasahi <asa1313to@gmail.com>
2022-11-10 10:07:30 +0900

    commit:指摘内容を訂正しました

--- Karuvege.ino
+++ Karuvege.ino
@@ -1,22 +1,21 @@
 #include <Wire.h>
 #include <ST7032.h>
-#include "getData.h"
+#include "Getdata.h"
 #include <HTTPClient.h>
-
-#define JST 3600*9 //日本標準時子午線
-#define uS_TO_S_FACTOR 1000000ULL //マイクロ->秒に変換係数
 
 //Wifi
 HTTPClient http;
 WiFiClientSecure client;
 
+ST7032 lcd;
+
+//日本標準時子午線
+const int JST=3600*9;
+const char *wd[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
 //WiFi設定
 const char ssid[] = "C0p2Ec2-WLAN";
 const char password[] = "4Emah5LdS";
-
-static const char *wd[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-
-ST7032 lcd;
 
 void setup() {
   Serial.begin(115200);
@@ -51,16 +50,14 @@ void loop() {
   unsigned long currentHour = getTime("hour");
   unsigned long currentMin = getTime("min");
   unsigned long currentDay = getTime("day");
-  //  unsigned long currentMin = t->tm_min;
 
+  //(1行目)
   lcd.setCursor(0, 0);
-  lcd.print(currentHour);
-  lcd.print(":");
-  lcd.print(currentMin);
-
-  Serial.print(currentDay);
-
-
+  if(currentMin < 9){
+   lcd.printf("%d:0%d", currentHour, currentMin); 
+  }else{
+    lcd.printf("%d:%d", currentHour, currentMin); 
+  }
   // (2行目)
   lcd.setCursor(0, 1);
   lcd.print(wd[currentDay]);
