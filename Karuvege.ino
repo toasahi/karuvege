291dc4c2a3499f505cf93f41d83f4f87b5ec6a47
2200454 <uerunosono@icloud.com>
2022-11-15 14:57:37 +0900

    温度センサの処理

--- Karuvege.ino
+++ Karuvege.ino
@@ -9,6 +9,9 @@ WiFiClientSecure client;
 
 ST7032 lcd;
 
+//ピン
+int voutPin = 25;
+
 //日本標準時子午線
 const int JST=3600*9;
 const char *wd[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
@@ -16,6 +19,23 @@ const char *wd[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 //WiFi設定
 const char ssid[] = "C0p2Ec2-WLAN";
 const char password[] = "d6ad418b63849f1d5c2fb20b3389c60787a9504bbe8900e77f4b6871fbc9da48";
+
+//温度センサの値を取得する関数
+void temp(){
+
+  // R1の電圧を取得
+  int reading = analogRead(voutPin);
+
+  // AD値をmVに変換
+  //float voltage = ((long)reading * VOLT * 1000) / ANALOG_MAX;
+
+  //温度を算出
+  //float temprature = (voltage - 424) / 6.25 //LM60BIZのデータシートにあった　VO = (+6.25 mV/°C × T °C) + 424 mV　という式を参考にしました。
+  //float temprature = (voltage - 424) / 6.25//T = ( Vo-424[mV] ) / 6.25[mV]
+
+  Serial.print(reading);
+  
+}
 
 void setup() {
   Serial.begin(115200);
@@ -61,4 +81,7 @@ void loop() {
   // (2行目)
   lcd.setCursor(0, 1);
   lcd.print(wd[currentDay]);
+
+  temp();
+  
 }
