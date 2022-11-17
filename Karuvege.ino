c7457d81f13bf0cc514fbe7de2ba40a8c6d951d9
toasahi <asa1313to@gmail.com>
2022-11-17 09:33:34 +0900

    update: アナログピンの出力を取得

--- Karuvege.ino
+++ Karuvege.ino
@@ -10,7 +10,7 @@ WiFiClientSecure client;
 ST7032 lcd;
 
 //ピン
-int voutPin = 25;
+int voutPin = 35;
 
 //日本標準時子午線
 const int JST=3600*9;
@@ -24,7 +24,7 @@ const char password[] = "d6ad418b63849f1d5c2fb20b3389c60787a9504bbe8900e77f4b687
 void temp(){
 
   // R1の電圧を取得
-  int reading = analogRead(voutPin);
+  float reading = analogRead(voutPin);
 
   // AD値をmVに変換
   //float voltage = ((long)reading * VOLT * 1000) / ANALOG_MAX;
@@ -33,12 +33,13 @@ void temp(){
   //float temprature = (voltage - 424) / 6.25 //LM60BIZのデータシートにあった　VO = (+6.25 mV/°C × T °C) + 424 mV　という式を参考にしました。
   //float temprature = (voltage - 424) / 6.25//T = ( Vo-424[mV] ) / 6.25[mV]
 
-  Serial.print(reading);
+  Serial.println(reading);
   
 }
 
 void setup() {
   Serial.begin(115200);
+  delay(1000);
   //WiFi接続
   Serial.print("Connecting to");
   Serial.print(ssid);
@@ -53,7 +54,7 @@ void setup() {
     }
   }
   Serial.println("Connected");
-
+//
   //日本時間の設定
   configTime( JST, 0, "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");
   configTzTime("JST-9", "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");
@@ -82,6 +83,7 @@ void loop() {
   lcd.setCursor(0, 1);
   lcd.print(wd[currentDay]);
 
-  temp();
-  
+  //temp();
+  Serial.println(analogRead(voutPin));
+  delay(1000);
 }
