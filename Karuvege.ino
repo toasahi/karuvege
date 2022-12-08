5ab74413ec686e0f5b513885e27b5b1dac53d869
Asahi <82006976+toasahi@users.noreply.github.com>
2022-12-08 09:57:53 +0900

    Feature/temperature sensor (#6)
    * 温度センサの処理
    
    * update: アナログピンの出力を取得
    
    Co-authored-by: 2200454 <uerunosono@icloud.com>

--- Karuvege.ino
+++ Karuvege.ino
@@ -9,6 +9,9 @@ WiFiClientSecure client;
 
 ST7032 lcd;
 
+//ピン
+int voutPin = 35;
+
 //日本標準時子午線
 const int JST = 3600 * 9;
 const char *wd[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
@@ -16,6 +19,24 @@ const char *wd[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 //WiFi設定
 const char ssid[] = "C0p2Ec2-WLAN";
 const char password[] = "d6ad418b63849f1d5c2fb20b3389c60787a9504bbe8900e77f4b6871fbc9da48";
+
+
+//温度センサの値を取得する関数
+void temp(){
+
+  // R1の電圧を取得
+  float reading = analogRead(voutPin);
+
+  // AD値をmVに変換
+  //float voltage = ((long)reading * VOLT * 1000) / ANALOG_MAX;
+
+  //温度を算出
+  //float temprature = (voltage - 424) / 6.25 //LM60BIZのデータシートにあった　VO = (+6.25 mV/°C × T °C) + 424 mV　という式を参考にしました。
+  //float temprature = (voltage - 424) / 6.25//T = ( Vo-424[mV] ) / 6.25[mV]
+
+  Serial.println(reading);
+  
+}
 
 const int moterPin = 16;
 
@@ -25,8 +46,10 @@ unsigned long currentDay;
 
 unsigned long currentTime[3];
 
+
 void setup() {
   Serial.begin(115200);
+  delay(1000);
   //WiFi接続
   Serial.print("Connecting to");
   Serial.print(ssid);
@@ -41,7 +64,7 @@ void setup() {
     }
   }
   Serial.println("Connected");
-
+//
   //日本時間の設定
   configTime( JST, 0, "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");
   configTzTime("JST-9", "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");
@@ -86,6 +109,9 @@ void loop() {
   // (2行目)
   lcd.setCursor(0, 1);
   lcd.print(wd[currentDay]);
+
+  //temp();
+  Serial.println(analogRead(voutPin));
 
   delay(1000);
   lcd.clear();
