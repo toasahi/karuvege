a9af128903d698793c81085e96653fd4a78725ee
toasahi <asa1313to@gmail.com>
2023-01-26 09:40:57 +0900

    change:LCDモニター

--- Karuvege.ino
+++ Karuvege.ino
@@ -10,21 +10,24 @@ extern void bme280_PROC();
 #include <Adafruit_BME280.h>
 #include <SSD1306.h>
 
-//#include "Getdata.h"
+#include "Getdata.h"
 #include "NumberOfDaysUsed.h"
+
+// 定数宣言
+#define SEALEVELPRESSURE_HPA (1013.25)    // bme280用定数
 
 //Wifi
 HTTPClient http;
 WiFiClientSecure client;
 
+Adafruit_BME280 bme; // I2C
+SSD1306  display(0x3c, 21, 22);
+
 const char *wd[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
 //WiFi設定
-//const char ssid[] = "C0p2Ec2-WLAN";
-//const char password[] = "d6ad418b63849f1d5c2fb20b3389c60787a9504bbe8900e77f4b6871fbc9da48";
-
-const char ssid[] = "HUMAX-01F2B";
-const char password[] = "djFhMaN5LWFJN";
+const char ssid[] = "C0p2Ec2-WLAN";
+const char password[] = "d6ad418b63849f1d5c2fb20b3389c60787a9504bbe8900e77f4b6871fbc9da48";
 
 const int moterPin = 16;
 const int temperatureFanPin = 17;
@@ -37,20 +40,14 @@ int timeStatus = 0;
 
 int currentTime[6];
 
-// 定数宣言
-#define SEALEVELPRESSURE_HPA (1013.25)    // bme280用定数
-
-// class宣言
-Adafruit_BME280 bme; // I2C
-SSD1306  display(0x3c, 21, 22); 
-
-// 変数宣言     "0123456789ABCDEF"
 unsigned long delayTime;
 unsigned status;
 static int value;     //analog値を代入する変数を定義
 static float Volts;
 static float FloatValue;
 unsigned static int Val_ad;
+
+const int ledPin = 18;
 
 void setup() {
   Serial.begin(115200);
@@ -63,7 +60,6 @@ void setup() {
   display.display();
 
   setup280();
-
 
   //WiFi接続
   Serial.print("Connecting to");
@@ -83,18 +79,18 @@ void setup() {
   //日本時間の設定
   configTime( 3600 * 9, 0, "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");
   configTzTime("JST-9", "ntp.nict.jp", "time.google.com", "ntp.jst.mfeed.ad.jp");
+  delay(5000);
+
   getCurrentTime(currentTime);
 
   EEPROM.begin(16);
 
   // 一旦WiFiの接続を止める
   WiFi.mode(WIFI_OFF);
-  delay(5000);
-  //  delay(5000);
 
-  //  saveNumberOfDaysUsed(timeKey, timeStatus, currentTime[3], currentTime[4], currentTime[5]);
-  //  delay(2000);
-  //  int days = getNumberOfDaysUsed(timeKey, currentTime[3], currentTime[4], currentTime[5]);
+  saveNumberOfDaysUsed(timeKey, timeStatus, currentTime[3], currentTime[4], currentTime[5]);
+  delay(2000);
+  int days = getNumberOfDaysUsed(timeKey, currentTime[3], currentTime[4], currentTime[5]);
 }
 
 void loop() {
@@ -104,15 +100,7 @@ void loop() {
 
 
   bme280_PROC();  // 環境センサメイン処理部
-  delay(500);
 
-  if (currentMin < 9) {
-    //    lcd.printf("%d:0%d", currentHour, currentMin);
-  } else {
-    //    lcd.printf("%d:%d", currentHour, currentMin);
-  }
-
-  delay(1000);
 }
 
 /* モーターを回転させる
@@ -169,18 +157,4 @@ void setup280() {
   display.drawString(8, 8, "BME280 Ready!");
   delay(500);
   delayTime = 500;
-}
-
-void getCurrentTime(int currentTime[6]) {
-  time_t t;
-  struct tm *tm;
-  t = time(NULL);
-  tm = localtime(&t);
-
-  currentTime[0] = tm->tm_hour;
-  currentTime[1] = tm->tm_min;
-  currentTime[2] = tm->tm_wday;
-  currentTime[3] = tm->tm_year + 1900;
-  currentTime[4] = tm->tm_mon + 1;
-  currentTime[5] = tm->tm_mday;
 }
